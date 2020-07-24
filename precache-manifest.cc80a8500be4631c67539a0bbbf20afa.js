acc77534948ffaf22b32cc2d5d43fbbf61d2737e
yoolsylva <yoolsylva@gmail.com>
2020-07-24 17:06:11 +0700

    CNR lucky

---
+++ precache-manifest.cc80a8500be4631c67539a0bbbf20afa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5929aa3dda8f628f52a9d25e3afe74b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "624bec16d42568033665",
+    "url": "/static/css/main.f53e5477.chunk.css"
+  },
+  {
+    "revision": "9a2a965e4bd470e6d3ce",
+    "url": "/static/js/2.7066f02f.chunk.js"
+  },
+  {
+    "revision": "6e58100473025f96c342f2a28fd33096",
+    "url": "/static/js/2.7066f02f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "624bec16d42568033665",
+    "url": "/static/js/main.49003a89.chunk.js"
+  },
+  {
+    "revision": "f64c0747a947c8aa62e3",
+    "url": "/static/js/runtime-main.beece60e.js"
+  }
+]);
